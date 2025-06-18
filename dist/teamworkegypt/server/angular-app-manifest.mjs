7c2261345ae2b971bf239c3670e93cf8c85ec883
chris <chris@jobsglobal.com>
2025-06-18 12:59:20 +0400

    feat: Add new footer and header components for TeamWork Egypt
    - Introduced a new footer component with contact information and quick links.
    - Added a new header component with navigation links and mobile menu functionality.
    - Updated the main server HTML to reflect changes in stylesheet and script references.
    - Modified the main server script to include new component paths for lazy loading.

--- dist/teamworkegypt/server/angular-app-manifest.mjs
+++ dist/teamworkegypt/server/angular-app-manifest.mjs
@@ -8,26 +8,26 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-XK5TVWVI.js",
-      "chunk-MZGDYHY2.js",
-      "chunk-LBMC5L5E.js"
+      "chunk-4W4FBBX5.js",
+      "chunk-HKUSCNLN.js",
+      "chunk-G4SDOEYA.js"
     ],
     "route": "/"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-5B7OKSSW.js",
-      "chunk-MZGDYHY2.js",
-      "chunk-LBMC5L5E.js"
+      "chunk-SKXSMDIL.js",
+      "chunk-HKUSCNLN.js",
+      "chunk-G4SDOEYA.js"
     ],
     "route": "/register"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-3VPUVC3U.js",
-      "chunk-LBMC5L5E.js"
+      "chunk-Y5BWSECJ.js",
+      "chunk-G4SDOEYA.js"
     ],
     "route": "/career-tips"
   },
@@ -39,11 +39,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 10946, hash: '5b08dacd391304fdaa6b2da47b6cee4b175c531a0293a0ef3ad139a3ef0ec68b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1002, hash: '9cbf7ab9c0b837ae5902a6aae3403649b7464c477f0a21bdf68182c4b5cea0f5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'register/index.html': {size: 42748, hash: '831b78d72c3a031ccad406d62f16132680b6fe1240cf35b672a246c8133dfe38', text: () => import('./assets-chunks/register_index_html.mjs').then(m => m.default)},
-    'career-tips/index.html': {size: 52818, hash: '4e7c51c3ce5dd65cbc25fd96e97da0bc7a38fa9f59306bfaa1357550d927b434', text: () => import('./assets-chunks/career-tips_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 78292, hash: 'a0dede4fad622b6936746dc48eea831e133a388b357967ee0f5a04e88affbda0', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-S5NBDWZS.css': {size: 43382, hash: 'EF3SpSt5fXE', text: () => import('./assets-chunks/styles-S5NBDWZS_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 10946, hash: 'c386b94c45b26c550c744984c153b14ea14899efea1b5420b5b6b5d573d8fc31', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1002, hash: '4fad50fa8a1c1966fa67535e98eac3b2c1f3f8e2abc25fe099ea57268ace6c17', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 78653, hash: '0f781a586c0a5fc0b1eaa28d78190b5d1fbbf7ba55f973b5f2a86ee9a29f4dad', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'register/index.html': {size: 43109, hash: 'ce689ef78ff7355dbe0a8177c13f90fd32eed52bb59a825ab0920b568b7447a3', text: () => import('./assets-chunks/register_index_html.mjs').then(m => m.default)},
+    'career-tips/index.html': {size: 52731, hash: '7a777d0756d5ee5e61edf43d20b8ae4be7f11fbea1fcd042d75bf112999c0402', text: () => import('./assets-chunks/career-tips_index_html.mjs').then(m => m.default)},
+    'styles-7TGMIE6M.css': {size: 43423, hash: 'jqiNQiKk0rk', text: () => import('./assets-chunks/styles-7TGMIE6M_css.mjs').then(m => m.default)}
   },
 };
