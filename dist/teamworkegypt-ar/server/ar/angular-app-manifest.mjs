5cc5dee5153d9980fabec9750e6fe00e15be1200
chris <chris@jobsglobal.com>
2025-06-20 12:31:48 +0400

    feat: Update registration component and server files for improved functionality and localization
    - Added a new chunk file for module imports in the server.
    - Updated server.mjs to include the new chunk file and adjusted import paths.
    - Refactored HTML in register-minimal.component.html to enhance localization keys and improve structure.
    - Changed i18n keys for various elements in the registration form for consistency.
    - Improved user feedback messages and placeholders in the registration form.

--- dist/teamworkegypt-ar/server/ar/angular-app-manifest.mjs
+++ dist/teamworkegypt-ar/server/ar/angular-app-manifest.mjs
@@ -8,90 +8,90 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-I6KL475B.js",
-      "chunk-LLDXFQGD.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-TQ6NLXQP.js",
+      "chunk-FMLBC4QJ.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-YEABPDRW.js",
-      "chunk-LLDXFQGD.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-TKMKXMTP.js",
+      "chunk-FMLBC4QJ.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar/register"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-U26E7XEW.js",
-      "chunk-6UFHXMTU.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-TIEH24FL.js",
+      "chunk-ZY44S3UN.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar/career-tips"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-TJH2F56U.js",
-      "chunk-6UFHXMTU.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-OGHW3YQV.js",
+      "chunk-ZY44S3UN.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar/career-tips/1"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-TJH2F56U.js",
-      "chunk-6UFHXMTU.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-OGHW3YQV.js",
+      "chunk-ZY44S3UN.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar/career-tips/2"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-TJH2F56U.js",
-      "chunk-6UFHXMTU.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-OGHW3YQV.js",
+      "chunk-ZY44S3UN.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar/career-tips/3"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-TJH2F56U.js",
-      "chunk-6UFHXMTU.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-OGHW3YQV.js",
+      "chunk-ZY44S3UN.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar/career-tips/4"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-TJH2F56U.js",
-      "chunk-6UFHXMTU.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-OGHW3YQV.js",
+      "chunk-ZY44S3UN.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar/career-tips/5"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-TJH2F56U.js",
-      "chunk-6UFHXMTU.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-OGHW3YQV.js",
+      "chunk-ZY44S3UN.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar/career-tips/6"
   },
   {
     "renderMode": 0,
     "preload": [
-      "chunk-TJH2F56U.js",
-      "chunk-6UFHXMTU.js",
-      "chunk-SN7RH6QP.js"
+      "chunk-OGHW3YQV.js",
+      "chunk-ZY44S3UN.js",
+      "chunk-2CO3LZRZ.js"
     ],
     "route": "/ar/career-tips/*"
   },
@@ -103,17 +103,17 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 11017, hash: 'f4122edf23bf149d7fc30060de01aaf0d7a919166d784dcd0bbe097ad7517ad7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1047, hash: '7016de4de9590416fd474d85db8a7a7ea392a9b4e77a00c0f99b5aa0471d4d5a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'register/index.html': {size: 43194, hash: '6dd6cf52d2190255c083d5ebcce79b98f7459c5e2f04f6ce9f6d5c9b26f05357', text: () => import('./assets-chunks/register_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 77758, hash: '621fb654a7559be430d68cc2dc1dbd5adb260dc6f2678d1dc8e24590733f1f0e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'career-tips/3/index.html': {size: 57285, hash: 'a846f96ffb55305b84d973a21ba02fa9fa1c7aff8507b2f8bb3a79b8b731839d', text: () => import('./assets-chunks/career-tips_3_index_html.mjs').then(m => m.default)},
-    'career-tips/4/index.html': {size: 57385, hash: '53f20810105d9f50e57f32c9b901b186de0e9f87412ae81e585289be15dee27b', text: () => import('./assets-chunks/career-tips_4_index_html.mjs').then(m => m.default)},
-    'career-tips/1/index.html': {size: 57517, hash: '65342b5cd0e6da5d0354dd4170cb601873125957e9c88eed7c7ab2c094fc01e1', text: () => import('./assets-chunks/career-tips_1_index_html.mjs').then(m => m.default)},
-    'career-tips/index.html': {size: 52089, hash: '2720ba36525f35c3f0312eb6709965d166d4608f78cd663bba169862b71ac5f9', text: () => import('./assets-chunks/career-tips_index_html.mjs').then(m => m.default)},
-    'career-tips/5/index.html': {size: 57477, hash: '5e92ce34498a9a00b9e39587e136d2b1daff16547633b55e35540f7a6e5fa803', text: () => import('./assets-chunks/career-tips_5_index_html.mjs').then(m => m.default)},
-    'career-tips/2/index.html': {size: 57468, hash: '8fbfcf9acab80106434684ce00304ea7e087116be0acbcdb49b03c7e216b55d9', text: () => import('./assets-chunks/career-tips_2_index_html.mjs').then(m => m.default)},
-    'career-tips/6/index.html': {size: 57586, hash: '0ea9c503c7900a24480c3ca339c8690cb04f1b6f3c47f9363e2171431033096e', text: () => import('./assets-chunks/career-tips_6_index_html.mjs').then(m => m.default)},
-    'styles.css': {size: 44640, hash: 'mN24GvBkKAA', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 11276, hash: 'cca61d73f998c9a595d0dadcd6cac3636d12825b604fce79baf47cdfd93c1b9f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1048, hash: '8bb8ed7871ea278d93ae7c10972fa6111661904e8e526b816adf76c2ca9a73c3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'career-tips/3/index.html': {size: 61052, hash: '3e8150dda12c7122e885689f423a50737f03986cfb573d0a37f001817827be3b', text: () => import('./assets-chunks/career-tips_3_index_html.mjs').then(m => m.default)},
+    'register/index.html': {size: 44295, hash: 'b1cc3d52ec736e57b2e83f8149cb8d270f8829b306acbf423251d5e7db5d8a84', text: () => import('./assets-chunks/register_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 109547, hash: 'b701f408ffea5fde302b54a60f53a93d6c1ad3c03ce89e31bec746ee1020ac6a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'career-tips/1/index.html': {size: 61341, hash: '2637aecd86928bc3132c079a5d70a46bc1dedb03811183328eb0a04f9a3188f5', text: () => import('./assets-chunks/career-tips_1_index_html.mjs').then(m => m.default)},
+    'career-tips/4/index.html': {size: 60844, hash: '8bdbf66cc69c807cfdbe78876458b27ac54f121952072334bb751df0b105b8e5', text: () => import('./assets-chunks/career-tips_4_index_html.mjs').then(m => m.default)},
+    'career-tips/6/index.html': {size: 61362, hash: 'b1dd7d64bbb55b5a4f28ccc6c66e4f0eef043827f6e460f5ff31c03c443161a2', text: () => import('./assets-chunks/career-tips_6_index_html.mjs').then(m => m.default)},
+    'career-tips/2/index.html': {size: 60796, hash: 'cf4d87400dcd3ad06831301da40aa93e01db3e70f06678fd7ea53c1159ce9f16', text: () => import('./assets-chunks/career-tips_2_index_html.mjs').then(m => m.default)},
+    'career-tips/5/index.html': {size: 61150, hash: 'fc9dedbf5f6f290f9fa3f5490690ffd6633f6ea898722e63ec187469e863e022', text: () => import('./assets-chunks/career-tips_5_index_html.mjs').then(m => m.default)},
+    'career-tips/index.html': {size: 54964, hash: '2c1f91abc895590d910916dc7e6f28a1d6fc08506395e4304cda469d09df3793', text: () => import('./assets-chunks/career-tips_index_html.mjs').then(m => m.default)},
+    'styles.css': {size: 53835, hash: 'ITSNXeOZ5k8', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
   },
 };
